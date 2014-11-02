f0228d57c457a71ceff77d5ee5eec575a9098bb9
DsSysWay <linuxserverlover@gmail.com>
2014-11-02 22:19:20 +0800

    add

--- backup/makepair.php
+++ backup/makepair.php
@@ -10,7 +10,7 @@ function isInObjList($applist,$packagename)
     for($x=0;$x<$length; $x++)
     {
         $item = $applist[$x];
-        if($item->packagename == $packagename)
+        if($item['packagename']== $packagename)
         {
             return TRUE; 
         }
@@ -19,16 +19,59 @@ function isInObjList($applist,$packagename)
 
 }
 
+//return value is json_encode  string
+function constructJsonUnit($user,$diff_applist,$all_applist)
+{
+    $diff_data = array();
+    $length = count($diff_applist);
+
+    for($i=0; $i<$length;$i++)
+    {
+        $item = $diff_applist[$i];
+        $unit = array('name'=>$item['name'],'version'=>$item['version'],'packagename'=>$item['packagename'],'versionname'=>$item['versionname']);
+        array_push($diff_data,$unit);
+    }
+
+    $data = array();
+    $length = count($all_applist);
+
+    for($i=0; $i<$length;$i++)
+    {
+        $item = $all_applist[$i];
+        $unit = array('name'=>$item['name'],'version'=>$item['version'],'packagename'=>$item['packagename'],'versionname'=>$item['versionname']);
+        array_push($data,$unit);
+    }
+
+    //还差返回username
+    $res = array('user'=>$user,'diff_applist'=>$diff_data,"all_applist"=>$data); 
+    $json_res = json_encode($res);
+    return $json_res;
+
+}
 
 $redis = new Redis();
 $redis->connect("127.0.0.1",6379);
 
 $queue = "queue";
 $user = $_POST['user'];
-
-$logName = "makepair.log";
-file_put_contents($logName,print_r($user,true),FILE_APPEND);
-
+if(empty($user))
+{
+    echo "user is empty";
+    exit("");
+}
+$logName = "pair_log";
+file_put_contents($logName,print_r((__LINE__).':'.$user.'|'."\r\n",true),FILE_APPEND);
+//第一步先查看下是否自己已经被匹配删了
+$result = $redis->get('pair'.$user);
+if(!empty($result))
+{
+    #已经被匹配上了呵呵直接返回
+    var_dump($result); 
+    echo result;
+    //删除匹配结果
+    $result = $redis->delete('pair'.$user);
+    exit();
+}
 $current_user = $redis->get($queue);
 if(empty($current_user))
 {
@@ -36,6 +79,11 @@ if(empty($current_user))
     echo $user."enter queue,just waiting others to make pair";
     exit("");
 }
+
+
+file_put_contents($logName,print_r((__LINE__).':'.'queue current user:'.$current_user."\r\n",true),FILE_APPEND);
+file_put_contents($logName,print_r((__LINE__).':'.'login user:'.$user."\r\n",true),FILE_APPEND);
+
 if($current_user == $user)
 {
     echo $user."has been in  queue,just waiting others to make pair";
@@ -47,58 +95,56 @@ if($current_user == $user)
 else
 {
     $I_single_have = array();
-   $my_applist_str = $redis->get($user); 
-   $obj_applist_str = $redis->get($user); 
-   $my_applist = unserialize($my_applist_str);
-   $obj_applist = unserialize($obj_applist_str);
-   file_put_contents($logName,print_r(count($my_applist),true),FILE_APPEND);
-   file_put_contents($logName,print_r(count($obj_applist),true),FILE_APPEND);
+    $my_applist_str = $redis->get($user); 
+    $obj_applist_str = $redis->get($current_user); 
+    $my_applist = json_decode($my_applist_str,true);
+    $obj_applist = json_decode($obj_applist_str,true);
 
-   $myLength = count($my_applist);
-   for($i = 0; $i < $myLength; $i++)
-   {
-       $item = $my_applist[$i];
-       $res =  isInObjList($obj_applist,$item->packagename);
-       if($res == FALSE)
-       {
-           array_push($I_single_have,$item); 
-       }
-   
-   }
+    $myLength = count($my_applist);
+    for($i = 0; $i < $myLength; $i++)
+    {
+        $item = $my_applist[$i];
+        $res =  isInObjList($obj_applist,$item['packagename']);
+        if($res == FALSE)
+        {
+            array_push($I_single_have,$item); 
+        }
 
-   $Obj_single_have = array();
-   $objLength = count($obj_applist);
-   for($k = 0; $k < $objLength; $k++)
-   {
-   
-       $item = $obj_applist[$k];
-       $res =  isInObjList($my_applist,$item->packagename);
-       if($res == FALSE)
-       {
-           array_push($Obj_single_have,$item); 
-       }
-   
-   }
+    }
 
-   file_put_contents($logName,print_r(count($I_single_have),true),FILE_APPEND);
-   file_put_contents($logName,print_r(count($Obj_single_have),true),FILE_APPEND);
+    $Obj_single_have = array();
+    $objLength = count($obj_applist);
+    for($k = 0; $k < $objLength; $k++)
+    {
 
-   #存到配对内存中
-   #
-   $my_pair_res = serialize($Obj_single_have); 
-   $redis->set('pair'.$user,$my_pair_res);
-   $obj_pair_res = serialize($I_single_have); 
-   $redis->set('pair'.$current_user,$obj_pair_res);
+        $item = $obj_applist[$k];
+        $res =  isInObjList($my_applist,$item['packagename']);
+        if($res == FALSE)
+        {
+            array_push($Obj_single_have,$item); 
+        }
 
-   $data = array();
-   $pairLength = count($Obj_single_have);
-   while($i= 0; $i < $pairLength; $i++)
-   {
-       $item = $Obj_single_have[$i];
-       $data[] = array('name'=>$item->name,'version'=>$iten->version,'packagename'=>$item->packagename,'versionname'=>$item->versionname);
-   }
-   //还差返回username
-   echo json_encode($data);
+    }
+
+
+    #存到配对内存中
+    #数据结构应该为：user，被配对对象的差异差异applist
+    #$my_pair_res = serialize($Obj_single_have); 
+    #$redis->set('pair'.$user,$my_pair_res);
+    #存储被匹配对象的匹配结果供匹配对象拉到
+    $pair_obj_res = constructJsonUnit($user,$I_single_have,$my_applist);
+    file_put_contents($logName,print_r((__LINE__).':'.'I_single_have:'.count($I_single_have)."\r\n",true),FILE_APPEND);
+    file_put_contents($logName,print_r((__LINE__).':'.'applist_by_makepair:'.$pair_obj_res."\r\n",true),FILE_APPEND);
+    $redis->set('pair'.$current_user,$pair_obj_res);
+
+
+
+   $final_result = constructJsonUnit($current_user,$Obj_single_have,$obj_applist);
+    file_put_contents($logName,print_r((__LINE__).':'.'Obj_single_have:'.count($Obj_single_have)."\r\n",true),FILE_APPEND);
+   file_put_contents($logName,print_r((__LINE__).':'.'applist_by_makepair:'.$final_result,true),FILE_APPEND);
+   echo $final_result;
+   //配对完毕，清理队列等待数据
+ #  $redis->delete($queue);
 
 
 }
--- backup/post_applist.php
+++ backup/post_applist.php
@@ -6,22 +6,21 @@ error_reporting(-1);
 
 
 
-class AppInfo{
-     private $name;
-     private $version;
-     private $packagename;
-     private $versionname;
-     //app  logo 的url 规则是：http://domain/$user/$name.png
-     //按照这种规则去拉取图片就行
-     function _construct($name,$version,$packagename,$versionname)
-     {
-         $this->name = $name;
-         $this->version = $version;
-         $this->packagename = $packagename;
-         $this->versionname = $versionname;
-     }
+#class AppInfo{
+#     private $name; #     private $version;
+#     private $packagename;
+#     private $versionname;
+#     //app  logo 的url 规则是：http://domain/$user/$name.png
+#     //按照这种规则去拉取图片就行
+#     function _construct($name,$version,$packagename,$versionname)
+#     {
+#         $this->name = $name;
+#         $this->version = $version;
+#         $this->packagename = $packagename;
+#         $this->versionname = $versionname;
+#     }
 
-}
+#}
 
 function saveImage($user,$content,$filename){
    $png = base64_decode($content); 
@@ -42,7 +41,7 @@ $user = $_POST['user'];
 $applist = array();
 
 
-file_put_contents($logName,print_r($user,true),FILE_APPEND);
+file_put_contents($logName,print_r($user."\r\n",true),FILE_APPEND);
 
 $length = count($list);
 
@@ -52,6 +51,7 @@ if(!file_exists("./".$user))
     mkdir("./".$user,0777,true);
 }
 
+file_put_contents($logName,print_r($length."\r\n",true),FILE_APPEND);
 for($x=0;$x<$length;$x++)
 {
     $item = $list[$x];
@@ -61,16 +61,25 @@ for($x=0;$x<$length;$x++)
     echo $value;
     saveImage($user,$item['icon'],$item['name']);
    # file_put_contents($logName,print_r($item['icon'],true),FILE_APPEND);
-    file_put_contents($logName,print_r($value,true),FILE_APPEND);
 
 
     //
-    $app = new AppInfo($item['name'],$item['version'],$item['packagename'],$item['versionname']);
+    $app = array('name'=>$item['name'],'version'=>$item['version'],'packagename'=>$item['packagename'],'versionname'=>$item['versionname']);
     array_push($applist,$app);
 }
 
+$backgroud = $_POST['wall'];
+if(!empty($backgroud))
+{
+    saveImage($user,$backgroud,"backgroud");
+    file_put_contents($logName,print_r((__LINE__).":".$backgroud,true),FILE_APPEND);
+}
 
-$array_string  = serialize($applist);
+#$store_list = array("list"=>$applist);
+$store_list = $applist;
+$array_string  = json_encode($store_list);
+
+file_put_contents($logName,print_r("store json: ".$array_string,true),FILE_APPEND);
 $redis->set($user,$array_string);
 
 ?>
